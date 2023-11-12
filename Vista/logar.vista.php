f93e3b4177544cb2d462d6805aeecd6573f5c959
pnavlani <p.navlani@sapalomera.cat>
2023-11-12 18:29:23 +0100

    Creació de Recaptcha - Vista Creem un Recaptcha i generem per google els seus claus i posem el seu script api i div en la vista

--- Vista/logar.vista.php
+++ Vista/logar.vista.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inciar la Sessió</title>
     <link rel="stylesheet" href="../Estils/formulari.css">
+    <!-- Script ReCaptcha -->
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <h1>Iniciar Sessió</h1>
@@ -16,6 +18,8 @@
         <input type="text" id="email" name="email" placeholder="Email">
         <label> Contrasenya</label>
         <input type="password" id="contrasenya" name="contrasenya" placeholder="Contrasenya">
+        <!-- Recaptcha -->
+        <div class="g-recaptcha" data-sitekey="6LfbtQwpAAAAADK7SFB31j2ivquNW3rQIVHsqw8L"></div>
 
         <input type="submit" value="Inciar Sessió" name="submit">
         <input type="button" value="Tornar enrere"  onclick="window.location.href='../index.php'"> 
