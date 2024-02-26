26234d28ad6f569dcf7ebd18d2b9d192c7a7ac5b
pnavlani <p.navlani@sapalomera.cat>
2024-02-26 01:03:50 +0100

    GitHub HybridAuth fet

--- Controlador/githubHybrid.php
+++ Controlador/githubHybrid.php
@@ -5,7 +5,7 @@ require '../hybridauth/src/autoload.php';
  require_once '../Model/connexio.php'; 
 
     $config = [
-        'callback' => 'http://localhost/Practiques/Servidor/Pt05_paras_navlani/Controlador/autentifica',
+        'callback' => 'http://localhost/Practiques/Servidor/Pt05_paras_navlani/Controlador/githubHybrid.php',
     
         'keys' => [
             'key' => '8c87a06a9e6fd9040b35',
@@ -14,7 +14,7 @@ require '../hybridauth/src/autoload.php';
     ];
     
     function alert(){
-        missatge("No s'ha pogut conectar amb el servidor de GitHub", "error");
+       echo "No s'ha pogut conectar amb el servidor de GitHub", " error";
     }
     /**
      * Funcio que comprova que el usuari i la contrasenya sigui correcte
@@ -28,15 +28,25 @@ require '../hybridauth/src/autoload.php';
            $github = new Hybridauth\Provider\GitHub($config);
            $github->authenticate();
            $usuari = $github->getUserProfile();
+           $token = $github->getAccessToken();
            $github->disconnect();
-           return $usuari;
+           return [$usuari,$token];
        } catch (Exception $e) {
            echo "Error: " . $e->getMessage();
        }
     }
     
-    $usuari = oauth($config);
-    githubBDD($pdo, $usuari->email, $usuari->displayName, $usuari->token);
+    $dades = oauth($config);
+    $usuari = $dades[0];
+    $token = $dades[1];
+
+    $pdo = conectar();
+    if ($usuari) {
+        githubBDD($pdo, $usuari->email, $usuari->displayName, $token);
+    } else {
+        alert();
+    }
+    
     
     function githubBDD($pdo, $email, $usuari, $token){
         try {
@@ -52,12 +62,14 @@ require '../hybridauth/src/autoload.php';
             $stmt = $pdo->prepare("INSERT INTO usuaris (usuari, email, token) VALUES (:usuari, :email, :token)");
             $stmt->bindValue(':usuari', $usuari);
             $stmt->bindValue(':email', $email);
-            $stmt->bindValue(':token', $token);
+            $stmt->bindValue(':token', $token['access_token']);
             $stmt->execute();
+            header("Location: usuari.php");
         }
             $_SESSION['usuari'] = $usuari;
             $_SESSION['email'] = $email;
             $_SESSION['token'] = $token;
+           
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
